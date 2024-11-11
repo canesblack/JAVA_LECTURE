251ed29331bb55c4931b3e775b341ff022bc0d62
jong960107 <jong960107@naver.com>
2024-11-11 12:39:52 +0900

    [2024.11.11]

---
+++ Java_Study/src/p1_ConditionalStatement/Main.java
@@ -0,0 +1,19 @@
+package p1_ConditionalStatement;
+
+public class Main {
+		 public static void main(String[] args) {
+			 int score = 50;
+			 
+			 if(score >= 90){
+				    System.out.println("A");	
+				}else if(score >= 80){
+				    System.out.println("B");
+				}else if(score >= 70){
+				    System.out.println("C");
+				}else if(score >= 60){
+				    System.out.println("D");
+				}else{
+				    System.out.println("E");
+				}
+		}
+}
